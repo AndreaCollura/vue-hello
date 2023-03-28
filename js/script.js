4ec49017ff312bde405b1584a376545bfeb7fe9b
AndreaCollura <andrea.collur@gmail.com>
2023-03-28 15:00:31 +0200

    add h1 & img

---
+++ js/script.js
@@ -0,0 +1,70 @@
+
+const {createApp} = Vue;
+
+createApp({
+    data(){
+        return {
+            titolo: 'Thank YOU!',
+            image:'/img/0_xkJgg-6HskYrQ3N7.webp',
+            txtprop: 'text-center gold',
+            imgprop : 'w-50'
+        }
+    }
+}).mount('#app');
+
+
+
+// const {createApp} = Vue;
+
+// createApp({
+//     data(){
+//         return {
+//             number1: 0,
+//             number2: 0,
+//             result: 0
+//         }
+//     },
+//     methods: {
+//         summ(){
+//             this.result = this.number1 + this.number2
+            
+//         }
+//     }
+// }).mount('#app');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
